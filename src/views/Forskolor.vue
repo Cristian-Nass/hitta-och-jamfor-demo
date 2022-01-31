f778674e207b68ffa574a5ef07a4be05ffcc2851
Cristian <cristian.tirdad@gmail.com>
2022-01-31 18:26:32 +0100

    implemented axios

--- src/views/Forskolor.vue
+++ src/views/Forskolor.vue
@@ -1,5 +1,37 @@
 <template>
   <div class="forskolor">
     <h1>Förskolor</h1>
+    <h1>{{test}}</h1>
   </div>
 </template>
+
+
+<script lang="ts">
+import { defineComponent, onMounted, ref } from 'vue';
+import axios from 'axios'
+
+const getData = (() => {    
+  axios
+    .get('https://jsonplaceholder.typicode.com/users')
+    .then((res) => console.log(res.data))
+    .catch((e) => console.log(e)
+    );
+});
+
+export default defineComponent({
+  name: 'Forskolor',
+  components: {
+  },
+  setup() {
+
+    const test = ref('Cristian')
+
+    onMounted(() => {
+      getData();
+    })
+    return {
+      test
+    }
+  }
+});
+</script>
