4c3aab1746433d1973af49c6768100f8efbb7137
Cristian <cristian.tirdad@gmail.com>
2022-01-31 23:43:11 +0100

    implemented images

--- src/components/SchoolSelector.vue
+++ src/components/SchoolSelector.vue
@@ -3,15 +3,15 @@
     <h1>{{ msg }}</h1>
   </div>
     <div style="display: flex; justify-content: space-evenly; ">
-    <router-link to="/forskolor"><div class="selected-skol">Forskolor och familjedaghem</div></router-link>
-    <router-link to="/grundskolor"><div class="selected-skol">Grundskolor</div></router-link>
+    <router-link to="/forskolor"><div class="selected-skol" id="pre-school"></div></router-link>
+    <router-link to="/grundskolor"><div class="selected-skol" id="elementry-school"></div></router-link>
   </div>
   <div>
     <h1></h1>
   </div>
     <div style="display: flex; justify-content: space-evenly; ">
-    <router-link to="/gymnasieskolor"><div class="selected-skol">Gymnasieskolor</div></router-link>
-    <router-link to="/vardOchOmsorgsboenden"><div class="selected-skol">Vård och omsorgsboenden</div></router-link>
+    <router-link to="/gymnasieskolor"><div class="selected-skol" id="high-school"></div></router-link>
+    <router-link to="/vardOchOmsorgsboenden"><div class="selected-skol" id="care-center"></div></router-link>
   </div>
 </template>
 
@@ -28,19 +28,34 @@ export default defineComponent({
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
- .selected-skol{
-   font-family: Verdana, Geneva, Tahoma, sans-serif;
-   font-size: 20px;
-   text-align: center;
-   padding-top: 20px;
-   width: 30vw;
-   height: 100px;
-   background-color: darkgreen;
-   color: seashell;
- }
+  .selected-skol{
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    text-align: center;
+    width: 520px;
+    min-height: 320px;
+    background-color: darkgreen;
+    color: seashell;
+    background-repeat: no-repeat;
+  }
 
- a {
-   text-decoration: none;
- }
+  a {
+    text-decoration: none;
+  }
 
+  #pre-school{
+    background-image: url('./../assets/images/pre-school.jpg');
+  }
+
+  #elementry-school{
+    background-image: url('./../assets/images/elementry-school.jpg');
+  }
+
+  #high-school{
+    background-image: url('./../assets/images/high-school.jpg');
+  }
+
+  #care-center{
+    background-image: url('./../assets/images/care-center.jpg');
+  }
+  
 </style>
