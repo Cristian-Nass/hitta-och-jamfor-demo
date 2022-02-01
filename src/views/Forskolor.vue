1aad2619ca303b3ad11d1df5a09f4fb0cb4170ae
Cristian <cristian.tirdad@gmail.com>
2022-02-01 12:48:00 +0100

    implemented Grid

--- src/views/Forskolor.vue
+++ src/views/Forskolor.vue
@@ -3,11 +3,13 @@
     <h1>Förskolor</h1>
     <h1>{{test}}</h1>
     <!-- <h1>{{testData}}</h1> -->
-    <ul id="example-1">
-    <li v-for="item in schoolsData" :key="item.id">
-      {{ item.name }} - {{ item.username }} - {{ item.email }}
-    </li>
-  </ul>
+    <div class="main-div">
+      <div class="child-div" v-for="item in schoolsData" :key="item.id">
+        <div>{{ item.name }}</div>
+        <div>{{ item.username }}</div>
+        <div>{{ item.email }}</div>
+      </div>
+  </div>
   </div>
 </template>
 
@@ -28,7 +30,7 @@ export default defineComponent({
   setup() {
 
     const test = ref('Cristian')
-    
+
     const schoolsState: Schools = reactive({
       data: [],
     });
@@ -54,3 +56,20 @@ export default defineComponent({
   }
 });
 </script>
+
+
+<style scoped>
+ .main-div{
+   display: grid;
+   grid-template-columns: 1fr 1fr 1fr 1fr;
+ }
+
+ .child-div{
+   font-family: Verdana, Geneva, Tahoma, sans-serif;
+   text-align: center;
+   height: 300px;
+   background-color: lightblue;
+   margin: 10px;
+   padding: 10px;
+ }
+</style>
