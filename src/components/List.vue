027390edc24030e16656d3917301d0658ded1ab2
Cristian <cristian.tirdad@gmail.com>
2022-02-16 20:22:37 +0100

    flex

--- src/components/List.vue
+++ src/components/List.vue
@@ -7,30 +7,30 @@
     </div>
   </div> -->
 
-  <div id="app">
-    {{Math.ceil(totalRows/perPage)}}
-  <div class="myList">
-    <div v-for="list in lists" :key="list.id">
-    {{ list.Name }}
+  <div style="display:flex; flex-direction: column">
+    <div>Somthing</div>
+    <div>Somthing Else</div>
+    
+    <div class="parent-grid-div">
+      <div class="child-grid-div" v-for="list in lists" :key="list.id">
+        <img :src="list.Picture" />
+        <div>{{ list.Name }}</div>
+        <div>{{ list.Area }}</div>
+      </div>
+
+      <div class="page-item pagination-button" style="display: flex">
+        <div class="page-link" style="width: 40px">p</div>
+        <div
+          class="page-link pagination-button"
+          v-for="item in Math.ceil(totalRows / perPage)"
+          :key="item"
+          @click="setCurrentPage(item)">
+          {{ item }}
+        </div>
+        <div class="page-link pagination-button" style="width: 40px">n</div>
+      </div>
+    </div>
   </div>
-
-
-  
-  <div class="page-item pagination-button" style="display: flex">
-    <div class="page-link" style="width: 40px">p</div>   
-    <div class="page-link pagination-button" v-for="item in Math.ceil(totalRows/perPage)" :key="item" @click="setCurrentPage(item)">{{item}}</div>   
-    <div class="page-link pagination-button" style="width: 40px">n</div>   
-  </div>
-
-
-  
-  <!-- <b-pagination
-     :total-rows="totalRows" 
-     v-model="currentPage"
-     :per-page="perPage"
-  /> -->
-  </div>
-</div>
 </template>
 
 <script lang="ts">
@@ -38,35 +38,31 @@ import {defineComponent, ref, PropType, computed} from 'vue';
 import {SchoolsEntites} from '@/functions/getData';
 
 export default defineComponent({
-  
   props: {
     dataSchools: Array as PropType<SchoolsEntites[]>,
   },
-  
+
   components: {},
   name: 'List.vue',
 
   setup(props) {
-
-    const schoolsData = computed(() => props.dataSchools)
+    const schoolsData = computed(() => props.dataSchools);
     const currentPage = ref(1);
-    const perPage = ref(5);
+    const perPage = ref(8);
 
     const lists = computed(() => {
       const items = schoolsData.value;
       return items?.slice(
         (currentPage.value - 1) * perPage.value,
         currentPage.value * perPage.value
-      )
+      );
     });
 
-    const setCurrentPage = (pageNumber: number)  => {
-      currentPage.value = (pageNumber)
-    }
+    const setCurrentPage = (pageNumber: number) => {
+      currentPage.value = pageNumber;
+    };
 
     const totalRows = computed(() => schoolsData.value?.length);
-    
-   
 
     return {
       schoolsData,
@@ -102,6 +98,7 @@ img {
 
 .pagination-button {
   width: 40px;
+  height: 40px;
   cursor: default;
 }
 
